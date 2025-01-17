5e08dfd6aa038c6823deea0964676931f36193fa
tccmd <tccmd52@gmail.com>
2025-01-17 23:15:25 +0900

    thymeleaf 레이아웃 /layout

--- src/main/java/com/example/butter/controller/HelloController.java
+++ src/main/java/com/example/butter/controller/HelloController.java
@@ -1,12 +1,34 @@
 package com.example.butter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    	@RequestMapping("/")
+
+    @RequestMapping("/")
 	public String hello() {
 		return "index";
+	}
+
+	@GetMapping("/main")
+    public String main() {
+        return "main/main";
+    }
+
+	@GetMapping("/layout")
+    public String layout() {
+        return "layout/default_layout";
+    }
+
+	@GetMapping("/test")
+    public String test() {
+        return "fragment/config";
+    }
+
+	@RequestMapping("/portfolio")
+	public String portfolio() {
+		return "portfolio";
 	}
 }
